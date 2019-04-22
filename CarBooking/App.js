5cb2cf3616fbbd61598fc2e329846f91df0f1df9
morini <nicholas.morin@wallawalla.edu>
2019-04-21 22:44:19 -0700

    added cars and location pin

--- CarBooking/App.js
+++ CarBooking/App.js
@@ -1,19 +1,38 @@
 /**
- * 9. App.js
+ * 10. App.js
  */
 
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Animated, Image, StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
 import GeoCoder from 'react-native-geocoder';
+
+import ConfirmationModal from './src/components/ConfirmationModal';
+import LocationPin from './src/components/LocationPin';
 import LocationSearch from './src/components/LocationSearch';
+import ClassSelection from './src/components/ClassSelection';
 
 type Props = {};
 export default class App extends React.Component<Props> {
   constructor(props) {
     super(props);
     this.state = {
+      confirmationModalVisible: false,
       position: null,
+      carLocations: [{
+        rotation: 79,
+        latitude: 42.415114,
+        longitude: -71.682174
+      },
+      {
+      	rotation: 83,
+        latitude: 42.417617,
+        longitude: -71.684728
+      },{
+      	rotation: 8,
+        latitude: 42.418060,
+        longitude: -71.680479
+      }]
     };
     this.llu = {
       latitude:    34.049434,
@@ -39,38 +58,72 @@ export default class App extends React.Component<Props> {
       latitudeDelta:  0.00922,
       longitudeDelta: 0.00421
     };
+    this.hometown = {
+      latitude:   42.416759,
+      longitude:  -71.6828468,
+      latitudeDelta:  0.00922,
+      longitudeDelta: 0.00421
+    }
+  }
+
+  _newEvent = () => {
+    this.setState({ position: null });
+    if(this.timeoutId) clearTimeout(this.timeoutId);
   }
 
   _onRegionChange = (region) => {
-    this.setState({ position: null });
-    if(this.timeoutId) clearTimeout(this.timeoutId);
-    console.log(region);
+    this._newEvent
+    
     const self = this;
-    this.timeoutId = setTimeout(async () => {
+    this.timeoutId = setTimeout(async function() {
       try {
         const point = { lat: region.latitude, lng: region.longitude };
         const positions = await GeoCoder.geocodePosition(point);
         self.setState({ position: positions[0] });
-        console.log(this, self);
+        
       } catch (err) {
         console.log(err)
       }
     }, 2000);
   }
 
+  _onBookingRequest = () => {
+    this.setState({
+      confirmationModalVisible: true
+    });
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
         <MapView
-          style={styles.fullScreenMap}
-          initialRegion={ this.rog }
-          onRegionChangeComplete={ this._onRegionChange }
-        />
+        style={styles.fullScreenMap}
+        initialRegion={ this.hometown }
+        onRegionChangeComplete={ this._onRegionChange }
+         >
+         {this.state.carLocations.map((carLocation, i) => (
+           <MapView.Marker
+             key={i}
+             coordinate={carLocation}
+           >
+             <Animated.Image 
+               style={[styles.car, {transform: [{ rotate: `${carLocation.rotation}deg` }]}]} 
+               source={require('./img/car.png')} 
+             />
+           </MapView.Marker>
+           ))}
+         </MapView>
         <LocationSearch 
           value={
             this.state.position && 
             (this.state.position.feature || this.state.position.formattedAddress)
           }
+        />
+        <LocationPin onPress={ this._onBookingRequest }/>
+        <ClassSelection/>
+        <ConfirmationModal 
+          visible={this.state.confirmationModalVisible} 
+          onClose={()=>{this.setState({confirmationModalVisible: false})}}
         />
       </View>
     );
@@ -84,5 +137,8 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0
+  },
+  car: {
+
   },
 });
