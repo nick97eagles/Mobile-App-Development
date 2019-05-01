3d3ba93be1c1d759e1c15d830a26367a56f81434
morini <nicholas.morin@wallawalla.edu>
2019-05-01 09:24:05 -0700

    added new screens for chat, profile, and serarch

---
+++ Messaging/src/screens/Chats.js
@@ -0,0 +1,38 @@
+/*
+ * 2. src/screens/Chats.js
+ */
+
+import React from 'react'
+import { View, Text, FlatList, ActivityIndicator } from 'react-native'
+import {
+  createStackNavigator,
+  createAppContainer
+} from 'react-navigation'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+class ChatList extends React.Component {
+  imgPlaceholder = 'https://cdn.pixabay.com/photo/2017/03/21/02/00/user-2160923_960_720.png'
+
+  render () {
+    return (
+      <View>
+        {
+          <ActivityIndicator style={{marginTop: 20}}/>
+        }
+      </View>
+    )
+  }
+}
+
+export default createStackNavigator({
+  Chats: {
+    screen: ChatList,
+    navigationOptions: ({navigation}) => ({
+      title: 'Chats',
+      tabBarLabel: 'Chats',
+      tabBarIcon: ({ tintColor }) => (
+        <icon name="comment-o" size={ 30 } color={ tintColor }/>
+      )
+    }),
+  },
+});
---
+++ Messaging/src/screens/Profile.js
@@ -0,0 +1,52 @@
+/*
+ * 2. src/screens/Profile.js
+ */
+
+import React, { PropTypes } from 'react'
+import { View, Image, Button, Text } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+class Profile extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'Profile',
+    tabBarIcon: ({ tintColor }) => (
+      <Icon name="user" size={30} color={tintColor}/>
+    ),
+  };
+
+  imgPlaceholder = 'https://cdn.pixabay.com/photo/2017/03/21/02/00/user-2160923_960_720.png'
+
+  onPressLogout() {
+    console.log('Logout pressed');
+  }
+
+  render () {
+    return (
+        <View style={{ padding: 20 }}>
+          {
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Image
+                  source={{uri: (this.props.users && this.props.users.avatar || this.imgPlaceholder)}}
+                  style={{
+                    width: 100, 
+                    height: 100, 
+                    borderRadius: 50, 
+                    margin: 20, 
+                    resizeMode: 'cover'
+                  }}
+                />
+                <Text style={{fontSize: 25}}>{
+                  'Abraham Lincoln'
+                }</Text>
+              </View>
+          }
+          <Button
+            onPress={this.onPressLogout.bind(this)}
+            title="Logout"
+          />
+        </View>
+    )
+  }
+}
+
+export default Profile;
