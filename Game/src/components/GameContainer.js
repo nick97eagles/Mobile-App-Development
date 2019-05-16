cf98713649ceb08b1a3dfbe39b4b3641be38024e
morini <nicholas.morin@wallawalla.edu>
2019-05-16 09:27:39 -0700

    added animation to game

--- Game/src/components/GameContainer.js
+++ Game/src/components/GameContainer.js
@@ -1,50 +1,111 @@
-// 1. src/components/GameContainer.js
+// 4. src/components/GameContainer.js
 
 import React, { Component } from "react";
 import { TouchableOpacity, Image, StyleSheet } from "react-native";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
+import * as Actions from "../actions";
 import { W, H } from "../constants";
+import Parrot from "./Parrot";
+import Ground from "./Ground";
+import RockUp from "./RockUp";
+import RockDown from "./RockDown";
 import Score from "./Score";
 import Start from "./Start";
 import StartAgain from "./StartAgain";
 import GameOver from "./GameOver";
 
-// these lines will move to sprites.js eventually
-import Sound from "react-native-sound";
-var coinSound = new Sound('coin.wav', Sound.MAIN_BUNDLE, (error) => {
-  if (error) {
-    console.log('failed to load the sound', error);
-    return;
-  }
-  // loaded successfully
-  console.log('loaded sound with duration in seconds: ' + coinSound.getDuration());
-});
-
 class Game extends Component {
-  start = () => {
-    console.log("start");
-    coinSound.play((success) => {
-      if (success) {
-        console.log('successfully finished playing');
-      } else {
-        console.log('playback failed due to audio decoding errors');
-      }
-    });
+  constructor() {
+    super();
+    this.animationFrameId = null;
+    this.time = new Date();
+  }
+
+  nextFrame() {
+    if (this.props.gameOver) return;
+    var elapsedTime = new Date() - this.time;
+    this.time = new Date();
+    this.props.tick(elapsedTime);
+    this.animationFrameId = requestAnimationFrame(this.nextFrame.bind(this));
+  }
+
+  start() {
+    cancelAnimationFrame(this.animationFrameId);
+    this.props.start();
+    this.props.bounce();
+    this.time = new Date();
+    this.setState({ gameOver: false });
+    this.animationFrameId = requestAnimationFrame(this.nextFrame.bind(this));
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if (nextProps.gameOver) {
+      this.setState({ gameOver: true });
+      cancelAnimationFrame(this.animationFrameId);
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return !nextState.gameOver;
   }
 
   render() {
-    const isStarted = false;
-    const gameOver = false;
-    const score = 0;
+    // obtain data from Redux
+    const {
+      rockUp,
+      rockDown,
+      ground,
+      ground2,
+      parrot,
+      isStarted,
+      gameOver,
+      bounce,
+      score
+    } = this.props;
+
     return (
       <TouchableOpacity
-        onPress = { this.start }
+        onPress = {
+          !isStarted || gameOver ? this.start.bind(this) : bounce.bind(this)
+        }
         style={styles.screen}
         activeOpacity={1}
       >
         <Image
           source={require("../../images/bg.png")}
           style={[styles.screen, styles.image]}
+        />
+        <RockUp
+          x={rockUp.position.x * W}
+          y={rockUp.position.y}
+          height={rockUp.size.height}
+          width={rockUp.size.width}
+        />
+        <Ground
+          x={ground.position.x * W}
+          y={ground.position.y}
+          height={ground.size.height}
+          width={ground.size.width}
+        />
+        <Ground
+          x={ground2.position.x * W}
+          y={ground2.position.y}
+          height={ground2.size.height}
+          width={ground2.size.width}
+        />
+        <RockDown
+          x={rockDown.position.x * W}
+          y={rockDown.position.y * H}
+          height={rockDown.size.height}
+          width={rockDown.size.width}
+        />
+        <Parrot
+          x={parrot.position.x * W}
+          y={parrot.position.y * H}
+          height={parrot.size.height}
+          width={parrot.size.width}
         />
         <Score score={score} />
         {!isStarted && <Start />}
@@ -66,4 +127,22 @@ const styles = StyleSheet.create({
   }
 });
 
-export default Game;
+function mapStateToProps(state) {
+  const sprites = state.gameReducer.sprites;
+  return {
+    parrot: sprites[0],
+    rockUp: sprites[1],
+    rockDown: sprites[2],
+    gap: sprites[3],
+    ground: sprites[4],
+    ground2: sprites[5],
+    score: state.gameReducer.score,
+    gameOver: state.gameReducer.gameOver,
+    isStarted: state.gameReducer.isStarted
+  };
+}
+function mapStateActionsToProps(dispatch) {
+  return bindActionCreators(Actions, dispatch);
+}
+
+export default connect(mapStateToProps, mapStateActionsToProps)(Game);
--- Game/src/reducers/game.js
+++ Game/src/reducers/game.js
@@ -1,27 +1,40 @@
-// 2. src/reducers/game.js
+// 4. src/reducers/game.js
+
+import {
+  sprites,
+  moveSprites,
+  checkForCollision,
+  getUpdatedScore,
+  bounceParrot
+} from "../sprites";
 
 const initialState = {
-    score: 0,
-    gameOver: false,
-    isStarted: false,
-  };
-  
-  export default (state = initialState, action) => {
-    switch (action.type) {
-      case "TICK":
-        return {
-          ...state,
-        };
-      case "BOUNCE":
-        return {
-          ...state,
-        };
-      case "START":
-        return {
-          ...initialState,
-          isStarted: true
-        };
-      default:
-        return state;
-    }
-  };
+  score: 0,
+  gameOver: false,
+  isStarted: false,
+  sprites
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case "TICK":
+      return {
+        ...state,
+        sprites: moveSprites(state.sprites, action.elapsedTime),
+        gameOver: checkForCollision(state.sprites[0], state.sprites.slice(1)),
+        score: getUpdatedScore(state.sprites, state.score)
+      };
+    case "BOUNCE":
+      return {
+        ...state,
+        sprites: bounceParrot(state.sprites)
+      };
+    case "START":
+      return {
+        ...initialState,
+        isStarted: true
+      };
+    default:
+      return state;
+  }
+};
