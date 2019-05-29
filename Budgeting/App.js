91d90224fb53785cdf3005064b735e3f0258b19f
morini <nicholas.morin@wallawalla.edu>
2019-05-29 09:28:58 -0700

    init new app

---
+++ Budgeting/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>welcome</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
